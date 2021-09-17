fd049e625bf78ee3049365db1cbc3a83ec68863e
Santy <santi.codev@gmail.com>
2021-09-17 14:47:27 +0200

    DEV: Primer commit

---
+++ src/test/java/com/santicodev/productsshop/ProductsshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santicodev.productsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
